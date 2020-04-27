c125b381983dc3164e5267543e059f86d63a67c7
Yili He <eli.he999@hotmail.com>
2020-04-27 16:53:15 +0800

    <fix>rewire babel plugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,17 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  config = injectBabelPlugin(
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
-	config,
-  );
-  return config;
-};
+const {
+    override,
+    fixBabelImports,
+    // addLessLoader,
+} = require("customize-cra");
+
+
+module.exports = override(
+    fixBabelImports("import", {
+        libraryName: "antd", libraryDirectory: "es", style: 'css' // change importing css to less
+    }),
+    // addLessLoader({
+    //   javascriptEnabled: true,
+    //   modifyVars: { "@primary-color": "#1DA57A" }
+    // })
+    // The "injectBabelPlugin" helper has been deprecated as of v2.0. You can use customize-cra plugins in replacement - https://github.com/arackaf/customize-cra#available-plugins
+);
